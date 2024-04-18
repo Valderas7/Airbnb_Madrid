fd16aa3e6479cba2b9c1877b813abb7552b2e09a
Valderas7 <valderas777@gmail.com>
2024-04-18 21:51:10 +0200

    docs: explicación model serving mejorada

--- deployment/local/local_deployment_mlflow.py
+++ deployment/local/local_deployment_mlflow.py
@@ -15,7 +15,7 @@ X_test = json.dumps({
         "columns": ["square_feet", "accommodates", "cleaning_fee", "beds", "availability_30", "bedrooms", "availability_60", "availability_90", "availability_365", "security_deposit", "bathrooms", "guests_included", "neighbourhood", "neighbourhood_group_cleansed", "property_type", "room_type", "amenities", "X", "Y", "Z", "date"], 
         "data": [
             [np.nan, 6, 60.0, 4.0, 30, 2.0, 60, 88, 359, 600.0, 2.0, 4, "Retiro", "Retiro", "Apartment", "Entire home/apt", 18, 4846242.12107298, -312605.06587381434, 4134833.3663519137, 2], 
-            [np.nan, 6, 60.0, 4.0, 30, 2.0, 60, 88, 359, 600.0, 2.0, 4, "Retiro", "Retiro", "Apartment", "Entire home/apt", 18, 4846242.12107298, -312605.06587381434, 4134833.3663519137, 2]
+            [np.nan, 6, 40.0, 2.0, 50, 1.0, 50, 58, 359, 600.0, 2.0, 4, "Retiro", "Retiro", "Apartment", "Entire home/apt", 18, 4846242.12107298, -312605.06587381434, 4134833.3663519137, 2]
             ]
     }
 })                          
